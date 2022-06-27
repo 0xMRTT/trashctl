2b0356cf692cdde7817a447625e3dc80d5ce53fc
0xMRTT <0xMRTT@tuta.io>
2022-06-27 13:41:43 +0200

    ✨ feat(cli): Add completion

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,7 @@
 use clap::{Command, AppSettings, Arg, ValueHint, value_parser};
 use clap_complete::{generate, Generator, Shell};
 use std::io;
+use std::process::exit;
 
 pub fn build_cli() -> Command<'static> {
     Command::new("example")
@@ -9,13 +10,43 @@ pub fn build_cli() -> Command<'static> {
              .help("some input file")
                 .value_hint(ValueHint::AnyPath),
         )
-       .arg(
-           Arg::new("generator")
-               .long("generate")
-               .value_parser(value_parser!(Shell)),
-       )
+    .arg(
+        Arg::new("completion")
+        .long("completion")
+        .help("Generate completion script")
+        .value_name("SHELL")
+    )
 }
 
 pub fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+}
+
+pub fn generate_completion(shell:&str) {
+    match shell {
+        "bash" => {
+            print_completions(Shell::Bash, &mut build_cli());
+            exit(0);
+        }
+        "zsh" => {
+            print_completions(Shell::Zsh, &mut build_cli());
+            exit(0);
+        },
+        "fish" => {
+            print_completions(Shell::Fish, &mut build_cli());
+            exit(0);
+        },
+        "powershell" => {
+            print_completions(Shell::PowerShell, &mut build_cli());
+            exit(0);
+        },
+        "elvish" => {
+            print_completions(Shell::Elvish, &mut build_cli());
+            exit(0);
+        },    
+        _ => {
+            println!("Unknown completion type");
+            exit(1);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 mod cli;
+
 fn main() {
     let matches = cli::build_cli().get_matches();
+
+    if matches.is_present("completion") {
+        cli::generate_completion(matches.value_of("completion").unwrap());
+    }
+        
 }
