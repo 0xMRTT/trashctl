87bfbabeb4ffb37f3bc16d741df8553c9cc237f7
0xMRTT <0xMRTT@tuta.io>
2022-06-27 14:11:30 +0200

    ✨ feat(rm): Add rm

---
+++ src/cmd/rm.rs
@@ -0,0 +1,14 @@
+use clap::{arg, App, Arg, ArgMatches};
+
+pub fn make_subcommand<'help>() -> App<'help> {
+    App::new("rm")
+        .about("Delete permanently a file or a directory from the trash")
+        .arg(arg!(--force "Skips confirmation prompts"))
+        .arg(
+            Arg::new("path")
+                .long("path")
+                .takes_value(true)
+                .help("The path to restore")
+                .required(false),
+        )
+}
