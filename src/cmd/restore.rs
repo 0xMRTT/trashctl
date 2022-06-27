4c6f81fdfe241cf182da9402ff5280df03432d89
0xMRTT <0xMRTT@tuta.io>
2022-06-27 14:07:46 +0200

    ✨ feat(restore): Add subcommand

---
+++ src/cmd/restore.rs
@@ -0,0 +1,14 @@
+use clap::{arg, App, Arg, ArgMatches};
+
+pub fn make_subcommand<'help>() -> App<'help> {
+    App::new("restore")
+        .about("Restore a file or a directory from trash")
+        .arg(arg!(--force "Skips confirmation prompts"))
+        .arg(
+            Arg::new("path")
+                .long("path")
+                .takes_value(true)
+                .help("The path to restore")
+                .required(false),
+        )
+}
