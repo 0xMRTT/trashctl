acdc9a251f72224e6a7089dee6ab944bdff1c392
0xMRTT <0xMRTT@tuta.io>
2022-06-28 09:31:20 +0200

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,35 @@
+//! A command line interface for trash
+//! Copyright (C) 2022 0xMRTT <0xMRTT@tuta.io>
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//! 
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//! 
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//!
+//! # `trashctl` lib
+//! 
+//! This module contains the library code for trashctl.
+//! There is a lot of tools in this library, you can reuse them in your own projects.
+//! 
+ 
 use std::env;
 use std::path::PathBuf;
 use dirs::home_dir;
 use dirs::data_local_dir;
 
+
+/// Returns the path to the trash directory if XDG_DATA_HOME is set.
+/// If not, returns an error. 
+/// 
+/// Do not use this function, use instead `home_trash_dir_path`.
 pub fn home_trash_dir_path_from_env() -> Result<PathBuf, ()> {
     if env::var("XDG_DATA_HOME").is_ok() {
         let mut trash_dir = PathBuf::from(env::var("XDG_DATA_HOME").unwrap());
@@ -13,6 +40,9 @@ pub fn home_trash_dir_path_from_env() -> Result<PathBuf, ()> {
     return Err(());
 }
 
+/// Returns the path to the trash directory.
+/// If XDG_DATA_HOME is set, return XDG_DATA_HOME/Trash.
+/// If not, return ~/.local/share/Trash.
 pub fn home_trash_dir_path() -> PathBuf {
     let mut trash_dir = PathBuf::new();
 
