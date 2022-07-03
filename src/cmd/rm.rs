9703bf41a13cc7e5de7e0adcbb24be39d7b09514
0xMRTT <0xMRTT@tuta.io>
2022-07-03 22:37:52 +0200

    Mutiple files in rm

--- src/cmd/rm.rs
+++ src/cmd/rm.rs
@@ -5,14 +5,13 @@ pub fn make_subcommand<'help>() -> App<'help> {
         .about("Delete permanently a file or a directory from the trash")
         .arg(arg!(--force "Skips confirmation prompts"))
         .arg(
-            Arg::new("path")
-                .long("path")
-                .takes_value(true)
-                .help("The path to restore")
-                .required(false),
+            Arg::new("files").required(true).min_values(1),
         )
 }
 
 pub fn execute(args: &ArgMatches) -> Result<(), ()> {
+
+    let files: Vec<_> = args.values_of("files").unwrap().collect();
+    
     Ok(())
 }
