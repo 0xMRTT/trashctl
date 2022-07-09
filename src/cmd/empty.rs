170cf059c749a98f5526883626aa6a0c4ae9e341
0xMRTT <0xMRTT@tuta.io>
2022-07-09 11:41:55 +0200

    Resolve minor error

--- src/cmd/empty.rs
+++ src/cmd/empty.rs
@@ -10,7 +10,7 @@ pub fn make_subcommand<'help>() -> App<'help> {
 pub fn execute(args: &ArgMatches) -> Result<(), ()> {
     let home_trash_dir_path = home_trash_dir_path();
     println!("{}", home_trash_dir_path.display());
-    let trash = Trash::auto_recon(home_trash_dir_path);
+    let mut trash = Trash::auto_recon(home_trash_dir_path);
     trash.empty();
 
     Ok(())
